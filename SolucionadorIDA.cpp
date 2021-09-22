f5dc544b69f37fff8d0f46939a665fdc66100efc
Fabiola Jiménez-González <45375479+faniajime@users.noreply.github.com>
2021-09-21 20:29:27 -0600

    IDA cascaron

---
+++ SolucionadorIDA.cpp
@@ -0,0 +1,39 @@
+#include "SolucionadorIDA.h"
+#include "Estado.h"
+#include "Solucion.h"
+#include "Lista.h"
+#include "Problema.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+SolucionadorIDA::SolucionadorIDA(){
+
+}
+
+Lista * SolucionadorIDA::revertPasos(){
+
+}
+
+Solucion * SolucionadorIDA::solucione( Problema * problema){
+
+
+
+}
+
+Solucion * SolucionadorIDA::retornarSolucion(){
+
+}
+
+void SolucionadorIDA::agregarAFrontera(Lista * siguientes){
+	
+}
+
+int SolucionadorIDA::comprobarSiSolucion(Problema* problema){
+
+}
+
+void SolucionadorIDA::expandir(Problema* problema){
+
+}
+
---
+++ SolucionadorIDA.h
@@ -0,0 +1,17 @@
+#ifndef SOLUCIONADOR_IDA
+#define SOLUCIONADOR_IDA
+#include "Solucionador.h"
+#include <iostream>
+#include <list>
+#include <iterator>
+
+using namespace std;
+
+class SolucionadorIDA : public Solucionador {
+   private:
+
+   public:
+      SolucionadorIDA();
+};
+
+#endif
